fc0e7941690e413c4052de6df39bd7aa5e7f3d8f
ninabuscemi <ngbuscemi@gmail.com>
2022-07-19 21:05:18 -0400

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ function menuToggle () {
   var x = document.getElementById('myNavtoggle');
 // Below changes class name based on existing class name, so that the proper version of the menu (shown or hidden) will be displayed.
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
